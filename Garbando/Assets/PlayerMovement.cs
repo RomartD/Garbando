be9ff9bf427d7b30912c470f79d219dce81c70fd
RomartD <99225552+RomartD@users.noreply.github.com>
2023-08-09 22:24:31 +1200

    Added jump movement (need to fix hold space)

--- Garbando/Assets/PlayerMovement.cs
+++ Garbando/Assets/PlayerMovement.cs
@@ -7,12 +7,15 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, world");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("space"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 10, 0);
+        }
     }
 }
