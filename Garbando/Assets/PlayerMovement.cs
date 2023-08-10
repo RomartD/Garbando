d80b39fac776fbd43128a95fde3fb38e16bcf724
RomartD <99225552+RomartD@users.noreply.github.com>
2023-08-10 20:24:14 +1200

    Fixed double jumping, happy with player movement so far

--- Garbando/Assets/PlayerMovement.cs
+++ Garbando/Assets/PlayerMovement.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private bool isGrounded;
 
-    
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isGrounded = true;
     }
 
     // Update is called once per frame
@@ -20,9 +21,16 @@ public class PlayerMovement : MonoBehaviour
 
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(0, 10f);
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            isGrounded = false;
         }
+    }
+
+    // Called when the player collides with any collider
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true; // Reset isGrounded when player collides with anything
     }
 }
