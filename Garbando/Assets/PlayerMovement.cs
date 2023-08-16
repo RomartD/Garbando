9cedaa3528e414ce09c8de81f59ac32767269aa0
RomartD <99225552+RomartD@users.noreply.github.com>
2023-08-16 19:56:45 +1200

    Terrain side sick bug fix

--- Garbando/Assets/PlayerMovement.cs
+++ Garbando/Assets/PlayerMovement.cs
@@ -1,36 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private bool isGrounded;
+    private BoxCollider2D coll;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        isGrounded = true;
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         float dirX = Input.GetAxisRaw("Horizontal");
-
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
-            isGrounded = false;
         }
     }
 
-    // Called when the player collides with any collider
-    private void OnCollisionEnter2D(Collision2D collision)
+    private bool IsGrounded()
     {
-        isGrounded = true; // Reset isGrounded when player collides with anything
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
+
